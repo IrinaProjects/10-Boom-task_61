ab37ea9ae29fa0501d22a57373e4c83db53ba868
Irina Spasova <irina.spasova.81@gmail.com>
2023-01-15 11:36:16 +0200

    solution 02

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,19 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-  let hotIconCheck = document.querySelectorAll(".hot");
+  // let hotIcon = document.querySelectorAll(".hot");
+  // console.log(hotIcon);
+  // hotIcon.forEach((element) => {
+  //   if (!element.textContent.includes("🔥")) {
+  //     element.textContent += "🔥";
+  //   }
+  // });
 
-  hotIconCheck.forEach((element) => {
-    if (!element.textContent.includes("🔥")) {
-      element.textContent += "🔥";
-    }
-  });
+  const nodeList = document.querySelectorAll(".hot");
+  for (let i = 0; i < nodeList.length; i++) {
+    nodeList[i].textContent += "🔥";
+  }
 });
+
+// Не вярвах, че ще доживея да го кажа, ма на моя компютър 52 задача,Hot Price, работи:
+// Има си и querySelectorAll, ама тестовете твърдят, че нищи си няма. Какво съм объркала? Мерси предварително! :)
