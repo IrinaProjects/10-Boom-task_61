210cac651c20d78ec3eb710e1115d7d44aaad883
Irina Spasova <irina.spasova.81@gmail.com>
2023-01-15 11:37:17 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,8 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+   const nodeList = document.querySelectorAll(".hot");
+  for (let i = 0; i < nodeList.length; i++) {
+    nodeList[i].textContent+="🔥";
+  }
 });
