4bab95592d326d4c121dab8ca1ea5c1ae9091f55
Irina Spasova <irina.spasova.81@gmail.com>
2023-01-15 11:16:02 +0200

    ready

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,11 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  let hotIconCheck = document.querySelectorAll(".hot");
+
+  hotIconCheck.forEach((element) => {
+    if (!element.textContent.includes("🔥")) {
+      element.textContent += "🔥";
+    }
+  });
 });
